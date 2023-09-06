a6d63ffd0d0e9c02fbf56361eda194a021abd03f
MikkoTirronen <MikkoATirronen@gmail.com>
2023-09-06 12:33:07 +0200

    added styling

--- packages/client/src/components/pages/AnogramPageComponent.jsx
+++ packages/client/src/components/pages/AnogramPageComponent.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import styled from 'styled-components'
-import WordInput from '../WordInput'
+import React, { useState } from "react";
+import styled from "styled-components";
+import WordInput from "../WordInput";
 
-  const Container = styled.div`
-  display: flexbox;
-  justify-content: center;
+const Container = styled.div`
+  display: inline-flex;
   flex-direction: column;
   background: grey;
-  width:  50vw;
+  width: 50vw;
   height: 100vh;
-  `
-  const Header = styled.h1`
+`;
+const Header = styled.h1`
   color: whitesmoke;
-  
-  `
-  const StyledButton = styled.button`
-  
-  `
-  const BodyContainer =  styled.div`
-  display: flex;
+`;
+const StyledButton = styled.button``;
+const BodyContainer = styled.div`
+  display: block;
   justify-content: center;
-  background: lightblue
-  `
+  background: lightblue;
+  border: solid;
+  padding: 10px;
+`;
+const Instructions = styled.p`
+  text-align: center;
+`;
 export default function AnogramPageComponent() {
-   
-
+  
+  
   return (
     <Container>
-<Header>Anogram Insanity</Header>
-<BodyContainer>
-
-<WordInput name ="wordOne"/>
+      <Header>Anogram Insanity</Header>
+      <BodyContainer>
+        
+        <WordInput name ="wordOne"/>
 <WordInput name ="wordTwo"/>
 
 <StyledButton>Submit</StyledButton>
-</BodyContainer>
+      </BodyContainer>
     </Container>
   )
- }
+}
