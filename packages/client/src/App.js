5752a03627d0cd45679020dc8e726b779fd957e1
MikkoTirronen <MikkoATirronen@gmail.com>
2023-09-06 17:03:23 +0200

    added puppies endpoint

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import AnogramPageComponent from './components/pages/AnogramPageComponent';
+import PuppiesComponentPage from './components/pages/PuppiesComponentPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AnogramPageComponent/>}/>
+      <Route path="/puppies" element={<PuppiesComponentPage/>}/>
     </Routes>
   );
 }
