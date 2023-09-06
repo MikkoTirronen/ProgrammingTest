866151f783b6ef0e593bc6a2fa9a78814269ad09
MikkoTirronen <MikkoATirronen@gmail.com>
2023-09-06 10:29:08 +0200

    added styling components

--- packages/client/src/components/AnogramPageComponent.jsx
+++ packages/client/src/components/AnogramPageComponent.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
+  const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  background: grey;
+  width: 100vw;
+  height: 100vh;
+  `
+  const Header = styled.h1`
+  color: whitesmoke;
+  
+  `
 export default function AnogramPageComponent() {
+   
+
   return (
-    <div>AnogramPageComponent</div>
+    <Container>
+<Header>Anogram Insanity</Header>
+    </Container>
   )
-}
+ }
