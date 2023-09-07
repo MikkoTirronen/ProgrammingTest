06d5de210873150f1269f5e3d035d1343b33ed22
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-09-07 19:10:33 +0200

    added navigation

---
+++ packages/client/src/components/NavigationComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function NavigationComponent() {
+  return (
+    <div>
+      <nav>
+        
+          <li>
+            <a href="/">Anogram Page</a>
+          </li>
+          <li>
+            <a href="/puppies">Puppies Page</a>
+          </li>
+          <li>
+            <a href="/dashboard">Dashboard</a>
+          </li>
+    
+      </nav>
+    </div>
+  );
+}
