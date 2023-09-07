2e54a9660c1ed29c664912be720d2b218169d0eb
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-09-07 23:44:23 +0200

    added database config

---
+++ packages/server/config.js
@@ -0,0 +1,11 @@
+const config = {
+  db: {
+    host: "127.0.0.1",
+    user: "cabin",
+    password: "cabinpw",
+    database: "cabin",
+    connectTimeout: 60000,
+  },
+  listPerPage: 10,
+};
+module.exports = config;
