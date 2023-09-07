7d1cf904e80078bf9283554fc4e21eee87a7114a
Mikko Tirronen <mikkoatirronen@gmail.com>
2023-09-07 19:10:17 +0200

    added frontend data and some formating

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import AnogramPageComponent from './components/pages/AnogramPageComponent';
-import PuppiesComponentPage from './components/pages/PuppiesComponentPage';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import AnogramPageComponent from "./components/pages/AnogramPageComponent";
+import PuppiesComponentPage from "./components/pages/PuppiesComponentPage";
+import DashboardPageComponent from "./components/pages/DashboardPageComponent";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AnogramPageComponent/>}/>
-      <Route path="/puppies" element={<PuppiesComponentPage/>}/>
+      <Route path="/" element={<AnogramPageComponent />} />
+      <Route path="/puppies" element={<PuppiesComponentPage />} />
+      <Route path="/dashboard" element={<DashboardPageComponent />} />
     </Routes>
   );
 }
